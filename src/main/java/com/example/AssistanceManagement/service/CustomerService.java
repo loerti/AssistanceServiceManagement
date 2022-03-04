a0852b18f3024d7d39ea0020e7775ca4670a7483
Loerti <loertishahini@gmail.com>
2022-03-04 19:00:13 +0100

    added costumerService

---
+++ src/main/java/com/example/AssistanceManagement/service/CustomerService.java
@@ -0,0 +1,38 @@
+package com.example.AssistanceManagement.service;
+
+import com.example.AssistanceManagement.model.CustomerModel;
+import com.example.AssistanceManagement.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public Optional<CustomerModel> findById(Integer id){
+        return customerRepository.findById(id);
+    }
+
+    public CustomerModel saveProduct(CustomerModel customerModel){
+        return customerRepository.save(customerModel);
+    }
+
+    public CustomerModel updateProduct (CustomerModel customerModel) {
+        CustomerModel existingCustomer = customerRepository.findById(customerModel.getCustomerId()).orElse(null);
+
+        existingCustomer.setCustomerEmail(customerModel.getCustomerEmail());
+        existingCustomer.setCustomerName(customerModel.getCustomerName());
+        existingCustomer.setCustomerSurname(customerModel.getCustomerSurname());
+        existingCustomer.setCustomerPhoneNumber(customerModel.getCustomerPhoneNumber());
+        return customerRepository.save(existingCustomer);
+    }
+
+    public void deleteCostumer(int id){
+        customerRepository.deleteById(id);
+    }
+
+}
